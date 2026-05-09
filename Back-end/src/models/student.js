648b5845135bef71a18d5f0f6d927ac02b1fede6
HoangPhanHa23ITEB028 <quocte2811@gmail.com>
2026-05-09 17:07:23 +0700

    fix  student model

--- Back-end/src/models/student.js
+++ Back-end/src/models/student.js
@@ -2,7 +2,11 @@ const { DataTypes } = require('sequelize');
 const { sequelize } = require('../config/database');
 
 const Student = sequelize.define('Student', {
-    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
 
     studentCode: {
         type: DataTypes.STRING(50),
@@ -13,9 +17,22 @@ const Student = sequelize.define('Student', {
     faceStatus: {
         type: DataTypes.STRING(20),
         allowNull: true,
+        field: 'face_status',
         validate: {
             isIn: [['confirm', 'pending', 'reject']]
         }
+    },
+
+    faceVideoUrl: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+        field: 'face_video_url'
+    },
+
+    rejectReason: {
+        type: DataTypes.STRING(255),
+        allowNull: true,
+        field: 'reject_reason'
     },
 
     name: {
