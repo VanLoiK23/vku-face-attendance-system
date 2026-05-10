69af847d1f11cd24326d66e13a82f78402e8b49f
HoangPhanHa23ITEB028 <quocte2811@gmail.com>
2026-05-10 12:05:55 +0700

    fix interface system + fix api code interface

--- Back-end/src/models/semester.js
+++ Back-end/src/models/semester.js
@@ -1,27 +1,31 @@
-const { DataTypes } = require('sequelize');
-const { sequelize } = require('../config/database');
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("../config/database");
 
-const Semester = sequelize.define('Semester', {
+const Semester = sequelize.define(
+  "Semester",
+  {
     id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
     },
     name: {
-        type: DataTypes.STRING(100),
-        allowNull: false
+      type: DataTypes.STRING(100),
+      allowNull: false,
     },
     start_date: {
-        type: DataTypes.DATEONLY, 
-        allowNull: false
+      type: DataTypes.DATEONLY,
+      allowNull: false,
     },
     is_active: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: false
-    }
-}, {
-    tableName: 'semesters',
-    timestamps: false 
-});
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+  },
+  {
+    tableName: "semesters",
+    timestamps: false,
+  },
+);
 
 module.exports = Semester;
