810ce9a82fd1e5ed99e5673979fe449302cba981
HoangPhanHa23ITEB028 <quocte2811@gmail.com>
2026-05-12 12:10:21 +0700

    add-dashboard-teacher

---
+++ Back-end/src/controllers/teacher/DashboardController.js
@@ -0,0 +1,33 @@
+const teacherService = require("../../services/DashboardTeacherService");
+
+const get_Dashboard = async (req, res) => {
+  try {
+    const teacherId = req.user.accountId;
+    if (req.user.role !== "teacher") {
+      return res.status(403).json({
+        success: false,
+        message: "Bạn không có quyền truy cập",
+      });
+    }
+    // hoặc test cứng:
+    // const teacherId = 1;
+
+    const data = await teacherService.getTeacherDashboard(teacherId);
+
+    return res.status(200).json({
+      success: true,
+      data,
+    });
+  } catch (error) {
+    console.error(error);
+
+    return res.status(500).json({
+      success: false,
+      message: "Lỗi server",
+    });
+  }
+};
+
+module.exports = {
+  get_Dashboard,
+};
